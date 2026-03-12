904fbe142473adab857e27e2007f1b77cc4df88e
jjschulz <johnnajanelle01@gmail.com>
2026-03-12 11:51:46 -0400

    init 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.fieldpak.scan',
   appName: 'DocumentScanner',
-  webDir: 'www'
+  webDir: 'www',
+  plugins:{
+    LiveUpdate:{
+      appId: 'de353bab-5223-40f1-b111-650cc8e23a0b',
+    }
+  }
 };
 
 export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { LiveUpdate } from '@capawesome/capacitor-live-update';
+import { App } from '@capacitor/app';
+import { DocumentScanner } from '@capacitor-mlkit/document-scanner';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +12,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  async ngOnInit() {
+    // await DocumentScanner.installGoogleDocumentScannerModule();
+     const { nextBundleId } = await LiveUpdate.sync();
+      if (nextBundleId) {
+        const shouldReload = confirm("A new update is available. Would you like to install it?");
+        if (shouldReload) {
+          await LiveUpdate.reload();
+        }
+      }
+  }
+
+  constructor(){
+    App.addListener("resume", async () => {
+      const { nextBundleId } = await LiveUpdate.sync();
+      if (nextBundleId) {
+        // Ask the user if they want to apply the update immediately
+        const shouldReload = confirm("A new update is available. Would you like to install it?");
+        if (shouldReload) {
+          await LiveUpdate.reload();
+        }
+      }
+    });
+  }
+
+  startDocumentScan(){
+    DocumentScanner.scanDocument({ resultFormats: 'PDF'}).then((result) => {
+      if (result.pdf) {
+        Share.share({ url: result.pdf.uri});
+      }
+      // result.pdfInfo; // { uri: string, pageCount: number }
+      // this.documentImageData = result.scannedImages;
+    }).then((result) => {
+      console.log('Document scan result:', result);
+    }).catch((error) => {
+      console.error('Error during document scan:', error);
+    });
+  }
 
 }
